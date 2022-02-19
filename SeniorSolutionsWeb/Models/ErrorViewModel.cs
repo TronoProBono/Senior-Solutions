0e829cfc02601e5958b2dfc0bff068c89c223d09
ValentineJoseph <joe.valentine22@gmail.com>
2022-02-19 10:11:49 -0500

    Initial Framework

---
+++ SeniorSolutionsWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SeniorSolutionsWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
