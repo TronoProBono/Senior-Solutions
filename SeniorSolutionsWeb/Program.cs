10917306654d47a4cd65be44b8648ea9e7c24760
Joseph Valentine <89618052+ValentineJoseph@users.noreply.github.com>
2022-02-19 16:54:22 -0500

    Razer page middleware added

--- SeniorSolutionsWeb/Program.cs
+++ SeniorSolutionsWeb/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
