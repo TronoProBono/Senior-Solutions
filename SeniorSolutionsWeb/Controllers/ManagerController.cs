1c11f7d4770961a428aa7c4b228ce7cefd60d692
0-Alex-B <09berrya@gmail.com>
2022-02-26 18:40:44 -0500

    - Includes Changes to the Main Page. * Added the Manager's Page along with its sub pages - Added the Controller for the Manager Page along with all sub pages

---
+++ SeniorSolutionsWeb/Controllers/ManagerController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeniorSolutionsWeb.Controllers
+{
+    public class ManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Service()
+        {
+            return View();
+        }
+        public IActionResult Schedule()
+        {
+            return View();
+        }
+        public IActionResult Transportation()
+        {
+            return View();
+        }
+
+        public IActionResult clubs()
+        {
+            return View();
+        }
+
+        public IActionResult events()
+        {
+            return View();
+        }
+
+        public IActionResult create_clubs()
+        {
+            return View();
+        }
+
+        public IActionResult create_events()
+        {
+            return View();
+        }
+
+        public IActionResult post()
+        {
+            return View();
+        }
+
+        public IActionResult Manage_Accounts()
+        {
+            return View();
+        }
+
+    }
+}
