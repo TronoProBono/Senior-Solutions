bbde7e423b0461c973f60900d997ac7792f8fa3f
0-Alex-B <09berrya@gmail.com>
2022-02-26 17:05:44 -0500

    Includes Changes to the Manager's Page. - Added the Controller for the Manager Page along with three sub pages (Index, Schedule, and Transportation)

---
+++ SeniorSolutionsWeb/Controllers/ManagerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeniorSolutionsWeb.Controllers
+{
+    public class ManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Service()
+        {
+            return View();
+        }
+        public IActionResult Schedule()
+        {
+            return View();
+        }
+        public IActionResult Tranportation()
+        {
+            return View();
+        }
+
+    }
+}
