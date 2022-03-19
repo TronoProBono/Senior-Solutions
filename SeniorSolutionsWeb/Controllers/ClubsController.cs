d0d8c065addb28e9f1ea22aa1e93c64a157feecd
0-Alex-B <09berrya@gmail.com>
2022-03-19 18:14:35 -0400

    - Added the layout for the Clubs Page & Made a Small Update to the General Layout Page

---
+++ SeniorSolutionsWeb/Controllers/ClubsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeniorSolutionsWeb.Controllers
+{
+    public class ClubsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
