38672d0d0b3812710a7e1171f0046bcc6232f57c
0-Alex-B <09berrya@gmail.com>
2022-03-28 15:56:32 -0400

    [This push involves changes to the database, please use 'Update-Database' when attempting to view the Clubs page the first time after pulling]
    Clubs Implementation [Part 1]
    * All changes rely in the newly added tables for Clubs
    * 4 Tables have been added to the Data Context Class
    * Modified the Clubs Controller to accept filtered information
    - [Known Bug: Doesn't accept 2 directional start and end times]
    - [Know Bug: Not all filters are complete and will be fixed in part 2]

--- SeniorSolutionsWeb/Controllers/ClubsController.cs
+++ SeniorSolutionsWeb/Controllers/ClubsController.cs
@@ -1,12 +1,84 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SeniorSolutionsWeb.Data;
 
 namespace SeniorSolutionsWeb.Controllers
 {
     public class ClubsController : Controller
     {
-        public IActionResult Index()
+        private readonly SeniorSolutionsWebContext _context;
+
+        public ClubsController(SeniorSolutionsWebContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int clubID)
+        {
+
+            var club = from Club in _context.Club
+                       where Club.ClubId == clubID
+                       select Club;
+            if (club.Count() == 0)
+            {
+                TempData["Error"] = "Club ID Not Valid";
+                return Redirect("/Clubs");
+            }
+            else
+            {
+                return View(club);
+            }
+
+            //return View(await _context.Club.ToListAsync());
+        }
+
+        public async Task<IActionResult> Index(string club_name, int location, string meeting_day, int start_time, int end_time)
+        {
+
+            var demi_Club = from Meet in _context.ClubMeeting
+                                 join Location in _context.Locations on Meet.MeetingPlace equals Location.LocationId
+                                 select new 
+                                 { 
+                                     ClubId = Meet.ClubId, 
+                                     LocationName = Location.LocationName, 
+                                     MeetingDay = Meet.MeetingDay, 
+                                     StartTime = Meet.StartTime, 
+                                     EndTime = Meet.EndTime
+                                 };
+            var club = from Club in _context.Club
+                            join Meet in demi_Club on Club.ClubId equals Meet.ClubId
+                            select new
+                            {
+                                ClubId = Club.ClubId,
+                                ClubName = Club.ClubName,
+                                DateClubCreated = Club.DateClubCreated,
+                                LocationName = Meet.LocationName,
+                                MeetingDay = Meet.MeetingDay,
+                                StartTime = Meet.StartTime,
+                                EndTime = Meet.EndTime
+                            };
+
+            if (!String.IsNullOrEmpty(club_name))
+            {
+                club = club.Where(s => s.ClubName!.Contains(club_name));
+            }
+
+            if (!(location < 0))
+            {
+                club = club.Where(s => s.!.Contains(location));
+            }
+
+            if (!String.IsNullOrEmpty(meeting_day))
+            {
+                club = club.Where(s => s.ClubName!.Contains(meeting_day));
+            }
+                        
+            if (!(start_time < 0) && !(end_time < 0))
+            {
+                club = club.Where(s => s.);
+            }
+
+            return View(await _context.Club.ToListAsync());
         }
     }
 }
