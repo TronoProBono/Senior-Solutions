ea98a72a3fa64fb23b6c7e47f1a497df84d4d110
Joseph Valentine <89618052+ValentineJoseph@users.noreply.github.com>
2022-04-06 16:34:36 -0400

    Db Setup

---
+++ SeniorSolutionsWeb/Models/Poll.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SeniorSolutionsWeb.Models
+{
+    public class Poll
+    {
+        public int Id { get; set; }
+        [MaxLength(200)]
+        public string Question { get; set; }
+        [MaxLength(200)]
+        public string Answer { get; set; }
+        public int Answer1Votes { get; set; }
+        [MaxLength(200)]
+        public string Answer2 { get; set; }
+        public int Answer2Votes { get; set; }
+        [MaxLength(200)]
+        public string Answer3 { get; set; }
+        public int Answer3Votes { get; set; }
+        [MaxLength(200)]
+        public string Answer4 { get; set; }
+        public int Answer4Votes { get; set; }
+        public List<PollVote> Votes { get; set; }
+    }
+}
