f6d0534d256dd54129b7ee1f7b915bf4a1a16c54
0-Alex-B <09berrya@gmail.com>
2022-04-14 22:41:20 -0400

    *Updated the Database to support NULL values

---
+++ SeniorSolutionsWeb/Migrations/20220415024036_RevisedNullID.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeniorSolutionsWeb.Migrations
+{
+    public partial class RevisedNullID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServiceRequest_Employee_EmployeeAssignedId",
+                table: "ServiceRequest");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmployeeAssignedId",
+                table: "ServiceRequest",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServiceRequest_Employee_EmployeeAssignedId",
+                table: "ServiceRequest",
+                column: "EmployeeAssignedId",
+                principalTable: "Employee",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ServiceRequest_Employee_EmployeeAssignedId",
+                table: "ServiceRequest");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmployeeAssignedId",
+                table: "ServiceRequest",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ServiceRequest_Employee_EmployeeAssignedId",
+                table: "ServiceRequest",
+                column: "EmployeeAssignedId",
+                principalTable: "Employee",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
