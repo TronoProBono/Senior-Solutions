0e48a27443721e515896f55c4e6cb0d6fb4dd1e0
0-Alex-B <09berrya@gmail.com>
2022-04-27 21:54:14 -0400

    * Removed the final foriegn keys

--- SeniorSolutionsWeb/Models/Invite.cs
+++ SeniorSolutionsWeb/Models/Invite.cs
@@ -11,25 +11,20 @@ namespace SeniorSolutionsWeb.Models
 
         [Display(Name = "Resident ID")]
         public int ResidentID { get; set; }
-        public Resident Resident { get; set; }
 
         [Display(Name = "Club ID")]
         public int? ClubID { get; set; }
-        public Club Club { get; set; }
 
         [Display(Name = "Club Role ID")]
         [ForeignKey("ClubRoles")]
         public int? RoleID { get; set; }
-        public ClubRoles Roles { get; set; }
 
         [Display(Name = "Event ID")]
         public int? EventID { get; set; }
-        public Event Event { get; set; }
 
         [Display(Name = "Event Role ID")]
         [ForeignKey("EventRoles")]
         public int? EventRoleID { get; set; }
-        public EventRoles EventRoles { get; set; }
 
     }
 }
